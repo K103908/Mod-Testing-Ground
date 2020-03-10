2fcc55c48e33a13e1712a06cf47cde29b1c6a38b
MEEP of Faith <meepmatthew@gmail.com>
2020-03-09 20:10:21 -0700

    add some ; and see what happens...

--- scripts/blocks/solarflare.js
+++ scripts/blocks/solarflare.js
@@ -3,11 +3,11 @@ const solarflare = new LaserTurret("solarflare");
 
 solarflare.shootType = extend(BulletType{
     Color tmpColor = new Color();
-    colors = [Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white]
-    tscales = [1, 0.7, 0.5, 0.2]
-    strokes = [2, 1.5, 1, 0.3]
-    lenscales = [1, 1.12, 1.15, 1.17]
-    float length = 560
+    colors = [Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white];
+    tscales = [1, 0.7, 0.5, 0.2];
+    strokes = [2, 1.5, 1, 0.3];
+    lenscales = [1, 1.12, 1.15, 1.17];
+    float length = 560;
 });
 
 solarflare.shootType.lifetime = 16;
