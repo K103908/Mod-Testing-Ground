b2a26b6db5ef5eb979de227ab6e914e15ad3677f
MEEP of Faith <meepmatthew@gmail.com>
2020-03-09 20:13:29 -0700

    used to be {}, try that

--- scripts/blocks/solarflare.js
+++ scripts/blocks/solarflare.js
@@ -3,10 +3,10 @@ const solarflare = new LaserTurret("solarflare");
 
 solarflare.shootType = extend(BulletType{
     Color tmpColor = new Color();
-    colors = [Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white];
-    tscales = [1, 0.7, 0.5, 0.2];
-    strokes = [2, 1.5, 1, 0.3];
-    lenscales = [1, 1.12, 1.15, 1.17];
+    colors = {Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white};
+    tscales = {1, 0.7, 0.5, 0.2};
+    strokes = {2, 1.5, 1, 0.3};
+    lenscales = {1, 1.12, 1.15, 1.17};
     float length = 560;
 });
 
