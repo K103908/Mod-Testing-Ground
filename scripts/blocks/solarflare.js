c4ccc50d4b92c0be7ad7779baa109faaacd90eab
MEEP of Faith <54301439+MEEPofFaith@users.noreply.github.com>
2020-03-12 09:39:57 -0700

    IDK what I'm doing at this point.

--- scripts/blocks/solarflare.js
+++ scripts/blocks/solarflare.js
@@ -1,12 +1,14 @@
 const solarflare = new LaserTurret("solarflare");
 
 solarflare.shootType = extend(BulletType, {
-    Color tmpColor = new Color();
-    colors = [Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white];
-    tscales = [1, 0.7, 0.5, 0.2];
-    strokes = [2, 1.5, 1, 0.3];
-    lenscales = [1, 1.12, 1.15, 1.17];
-    length = 560;
+    {
+        Color tmpColor = new Color();
+        colors = [Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white];
+        tscales = [1, 0.7, 0.5, 0.2];
+        strokes = [2, 1.5, 1, 0.3];
+        lenscales = [1, 1.12, 1.15, 1.17];
+        length = 560;
+    },
     
     {
         hitEffect = Fx.hitMeltdown;
@@ -15,21 +17,21 @@ solarflare.shootType = extend(BulletType, {
         drawSize = 420;
         lifetime = 16;
         pierce = true;
-    }
+    },
     
     update(b){
         if(b.timer.get(1, 5)){
             Damage.collideLine(b, b.getTeam(), hitEffect, b.x, b.y, b.rot(), length, true);
         };
         Effects.shake(1, 1, b.x, b.y);
-    }
+    },
     
     hit(b, var hitx, var hity){
         Effects.effect(hitEffect, colors[2], hitx, hity);
         if(Mathf.chance(0.4)){
             Fire.create(world.tileWorld(hitx + Mathf.range(5), hity + Mathf.range(5)));
         };
-    }
+    },
     
     draw(b){
         var baseLen = (length) * b.fout();
@@ -41,9 +43,9 @@ solarflare.shootType = extend(BulletType, {
                 Lines.stroke((9 + Mathf.absin(Time.time(), 0.8, 1.5)) * b.fout() * strokes[s] * tscales[i]);
                 Lines.lineAngle(b.x + Tmp.v1.x, b.y + Tmp.v1.y, b.rot(), baseLen * lenscales[i], CapStyle.none);
             };
-        }
+        },
     Draw.reset();
-    }
+    },
 });
 
 solarflare.shootType.damage = 2;
