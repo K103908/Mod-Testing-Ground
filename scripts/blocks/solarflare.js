74b2e877eae593528076d923035938140dd66283
MEEP of Faith <54301439+MEEPofFaith@users.noreply.github.com>
2020-04-03 01:12:13 -0700

    Ive learned z bit

--- scripts/blocks/solarflare.js
+++ scripts/blocks/solarflare.js
@@ -1,51 +1,22 @@
 const solarflare = new LaserTurret("solarflare");
 
-solarflare.shootType = extend(BulletType, {
+const solarflare.shootType = extendContent(meltdownLaser, "solar-beam", {
     {
-        Color tmpColor = new Color();
-        colors = [Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white];
-        tscales = [1, 0.7, 0.5, 0.2];
-        strokes = [2, 1.5, 1, 0.3];
-        lenscales = [1, 1.12, 1.15, 1.17];
-        length = 560;
+        this.Color tmpColor = new Color();
+        this.colors = [Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white];
+        this.tscales = [1, 0.7, 0.5, 0.2];
+        this.strokes = [2, 1.5, 1, 0.3];
+        this.lenscales = [1, 1.12, 1.15, 1.17];
+        this.length = 560;
     },
     
     {
-        hitEffect = Fx.hitMeltdown;
-        despawnEffect = Fx.none;
-        hitSize = 4;
-        drawSize = 420;
-        lifetime = 16;
-        pierce = true;
-    },
-    
-    update(b){
-        if(b.timer.get(1, 5)){
-            Damage.collideLine(b, b.getTeam(), hitEffect, b.x, b.y, b.rot(), length, true);
-        };
-        Effects.shake(1, 1, b.x, b.y);
-    },
-    
-    hit(b, var hitx, var hity){
-        Effects.effect(hitEffect, colors[2], hitx, hity);
-        if(Mathf.chance(0.4)){
-            Fire.create(world.tileWorld(hitx + Mathf.range(5), hity + Mathf.range(5)));
-        };
-    },
-    
-    draw(b){
-        var baseLen = (length) * b.fout();
-        Lines.lineAngle(b.x, b.y, b.rot(), baseLen);
-        for(int s = 0; s < colors.length; s++){
-            Draw.color(tmpColor.set(colors[s]).mul(1f + Mathf.absin(Time.time(), 1f, 0.1f)));
-            for(int i = 0; i < tscales.length; i++){
-                Tmp.v1.trns(b.rot() + 180, (lenscales[i] - 1) * 35);
-                Lines.stroke((9 + Mathf.absin(Time.time(), 0.8, 1.5)) * b.fout() * strokes[s] * tscales[i]);
-                Lines.lineAngle(b.x + Tmp.v1.x, b.y + Tmp.v1.y, b.rot(), baseLen * lenscales[i], CapStyle.none);
-            };
-        },
-    Draw.reset();
-    },
+        this.hitEffect = Fx.hitMeltdown;
+        this.damage = 2;
+        this.despawnEffect = Fx.none;
+        this.hitSize = 4;
+        this.drawSize = 420;
+        this.lifetime = 16;
+        this.pierce = true;
+    }
 });
-
-solarflare.shootType.damage = 2;
