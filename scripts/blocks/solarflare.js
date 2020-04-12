e619e90253ce7f43be13b2c89fb752384d53e656
MEEP of Faith <54301439+MEEPofFaith@users.noreply.github.com>
2020-04-12 06:03:53 -0700

    Makes sense to me

--- scripts/blocks/solarflare.js
+++ scripts/blocks/solarflare.js
@@ -1,6 +1,6 @@
 const solarflare = new LaserTurret("solarflare");
 
-const solarflare.shootType = extendContent(meltdownLaser, "solar-beam", {
+const solarflare.shootType = extend(meltdownLaser, {
     {
         this.Color tmpColor = new Color();
         this.colors = [Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white];
@@ -8,13 +8,15 @@ const solarflare.shootType = extendContent(meltdownLaser, "solar-beam", {
         this.strokes = [2, 1.5, 1, 0.3];
         this.lenscales = [1, 1.12, 1.15, 1.17];
         this.length = 560;
+        
+        {
+            this.hitEffect = Fx.hitMeltdown;
+            this.despawnEffect = Fx.none;
+            this.damage = 2;
+            this.hitSize = 4;
+            this.drawSize = 420;
+            this.lifetime = 16
+            this.pierce = true;
+        }
     }
 });
-
-solarflare.shootType.hitEffect = Fx.hitMeltdown;
-solarflare.shootType.damage = 2;
-solarflare.shootType.despawnEffect = Fx.none;
-solarflare.shootType.hitSize = 4;
-solarflare.shootType.drawSize = 420;
-solarflare.shootType.lifetime = 16;
-solarflare.shootType.pierce = true;
