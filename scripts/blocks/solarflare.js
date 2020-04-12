2fcee299fc08b76ff962ec99a13cfd0984604850
MEEP of Faith <meepmatthew@gmail.com>
2020-04-12 15:33:41 -0700

    What about this

--- scripts/blocks/solarflare.js
+++ scripts/blocks/solarflare.js
@@ -1,14 +1,12 @@
 const solarflare = new LaserTurret("solarflare");
 
 const solarflare.shootType = extend(meltdownLaser, {
-    {
-        var Color tmpColor = new Color();
-        var colors = [Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white];
-        var tscales = [1, 0.7, 0.5, 0.2];
-        var strokes = [2, 1.5, 1, 0.3];
-        var lenscales = [1, 1.12, 1.15, 1.17];
-        var length = 560;
-    }
+    var Color tmpColor = new Color();
+    var colors = [Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white];
+    var tscales = [1, 0.7, 0.5, 0.2];
+    var strokes = [2, 1.5, 1, 0.3];
+    var lenscales = [1, 1.12, 1.15, 1.17];
+    var length = 560;
 });
 
 solarflare.shootType.hitEffect = Fx.hitMeltdown;
