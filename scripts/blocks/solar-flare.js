67ab19f3e959ee16600a89ec89237ca9794ab4ee
MEEP of Faith <meepmatthew@gmail.com>
2020-04-13 15:07:46 -0700

    Maybe not triple burny burn

--- scripts/blocks/solar-flare.js
+++ scripts/blocks/solar-flare.js
@@ -20,9 +20,6 @@ solarflare.shootType = extend(BasicBulletType, {
     hit(b,hitx,hity){
         Effects.effect(Fx.hitMeltdown, colors[2], hitx, hity);
         if(Mathf.chance(0.8)){
-            //TRIPLE FLAMES BURNY BURN
-            Fire.create(world.tileWorld(hitx + Mathf.range(5f), hity + Mathf.range(5f)));
-            Fire.create(world.tileWorld(hitx + Mathf.range(5f), hity + Mathf.range(5f)));
             Fire.create(world.tileWorld(hitx + Mathf.range(5f), hity + Mathf.range(5f)));
         }
     },
