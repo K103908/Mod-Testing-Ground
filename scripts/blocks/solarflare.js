1a9b0e24c2b00ba5c504fff956dfe987c02ff444
MEEP of Faith <meepmatthew@gmail.com>
2020-04-13 11:11:18 -0700

    Copy some code.

--- scripts/blocks/solarflare.js
+++ scripts/blocks/solarflare.js
@@ -1,11 +1,38 @@
+//Large amounts of code copied from laser-interceptor.js from younggam/more-powerful-units. Once I know what I'm doing, I'll do stuff myself.
 const solarflare = new LaserTurret("solarflare");
 
-const solarflare.shootType = extend(meltdownLaser, {
-    this.colors = [e6c04555, f7d95eaa, ffec6e, ffffff];
-    this.tscales = [1, 0.7, 0.5, 0.2];
-    this.strokes = [2, 1.5, 1, 0.3];
-    this.lenscales = [1, 1.12, 1.15, 1.17];
-    this.length = 560;
+var tmpColor = new Color();
+var colors = [Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white];
+var tscales = [1, 0.7, 0.5, 0.2];
+var strokes = [2, 1.5, 1, 0.3];
+var lenscales = [1, 1.12, 1.15, 1.17];
+var length = 560;
+
+const solarflare.shootType = extend(BasicBulletType, {
+  init(b){
+    if (b == null) return;
+    Damage.collideLine(b, b.getTeam(), Fx.hitMeltdown, b.x, b.y, b.rot(), length, true);
+    //Look in damage.java for how this works, it's simular to lightning.
+    Effects.shake(1, 1, b.x, b.y);
+  },
+  hit(b,hitx,hity){
+    Effects.effect(Fx.hitMeltdown,colors[2],hitx,hity);
+
+  },
+  draw(b){
+    baseLen = (length) * b.fout();
+
+    Lines.lineAngle(b.x,b.y,b.rot(),baseLen);
+    for(var s = 0; s < colors.length; s++){
+      Draw.color(tmpColor.set(colors[s]).mul(1+Mathf.absin(Time.time(),1,0.1)));
+      for(var i = 0; i < tscales.length; i++){
+        Tmp.v1.trns(b.rot()+180,(lenscales[i]-1)*35);
+        Lines.stroke((4+Mathf.absin(Time.time(),0.8,1.5))*b.fout()*strokes[s]*tscales[i]);
+        Lines.lineAngle(b.x+Tmp.v1.x,b.y+Tmp.v1.y,b.rot(),baseLen*lenscales[i],CapStyle.none);
+      }
+    }
+    Draw.reset();
+  }
 });
 
 solarflare.shootType.hitEffect = Fx.hitMeltdown;
