b40ca9991f4afa895846452e2e2b67a02e829280
MEEP of Faith <meepmatthew@gmail.com>
2020-04-13 18:06:09 -0700

    Pls no instacrash

--- scripts/blocks/solar-flare.js
+++ scripts/blocks/solar-flare.js
@@ -19,9 +19,6 @@ solarflare.shootType = extend(BasicBulletType, {
         }
         Effects.shake(1, 1, b.x, b.y);
     },
-    hit(b,hitx,hity){
-        Effects.effect(this.hitEffect,this.colors[2],hitx!=null?hitx:b.x,hity!=null?hity:b.y);
-    },
     draw(b){
         baseLen = (length) * b.fout();
 
